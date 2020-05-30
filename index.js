9b799d1644fbad6a240d59126176216d2d4b43a2
Santiago Enciso <whereispolaris@gmail.com>
2020-05-30 17:27:34 -0500

    Created variables for Chromium to open a new page and go to the specified URL.

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ async function getItems() {
 		// fix viewport bug
 		defaultViewport: null,
 	});
+
+	// Opens new tab
+	const page = await browser.newPage();
+
+	// Store URL in a variable
+	const url = 'https://austin.craigslist.org/search/sss?query=ps4&sort=rel';
+
+	await page.goto(url);
 }
 
 getItems();
